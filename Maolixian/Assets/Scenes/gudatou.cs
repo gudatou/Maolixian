afedb18fd67453a1b940dc8c1354acc2037bf7e5
1351308275 <52514305+1351308275@users.noreply.github.com>
2019-08-26 14:31:51 +0800

    2
    222

--- Maolixian/Assets/Scenes/gudatou.cs
+++ Maolixian/Assets/Scenes/gudatou.cs
@@ -7,7 +7,7 @@ public class gudatou : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("李文涛");
+        Debug.Log("李文涛..");
     }
 
     // Update is called once per frame

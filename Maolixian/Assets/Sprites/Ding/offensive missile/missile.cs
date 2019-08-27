7cef81bcc702e2b0f4aa46ba51eab290a839bc28
Dingyifan <52514298+gudatou@users.noreply.github.com>
2019-08-27 11:51:42 +0800

    anjianbo daodan
    按键波 导弹

---
+++ Maolixian/Assets/Sprites/Ding/offensive missile/missile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rig;
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.AddForce(new Vector3(speed, 0,0));
+    }
+}

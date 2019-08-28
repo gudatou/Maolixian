a3a80de27559b69d3397da33e8801e8c2d412c7e
Dingyifan <52514298+gudatou@users.noreply.github.com>
2019-08-28 17:37:43 +0800

    Black cat
    Black cat

---
+++ Maolixian/Assets/Sprites/Ding/Black Cat/cat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    public float force = 10.0f;
+    private Rigidbody2D rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rd.AddForce(new Vector3(0, force, 0));
+        }
+    }
+
+}

167f2937fc9d2923ff9c77cf4b3a59e66c553444
Dingyifan <52514298+gudatou@users.noreply.github.com>
2019-08-29 11:56:15 +0800

    check
    check

--- Maolixian/Assets/Sprites/Ding/Black Cat/cat.cs
+++ Maolixian/Assets/Sprites/Ding/Black Cat/cat.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class cat : MonoBehaviour
 {
-    public float force = 10.0f;
+    public float force = 50.0f;
     private Rigidbody2D rd;
+    //public Vector3 dir;
+
     // Start is called before the first frame update
     void Start()
     {
+
         rd = this.GetComponent<Rigidbody2D>();
+        //di = GameObject.FindGameObjectsWithTag("Che");
+        //dir = di.position.y - transform.position.x;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             rd.AddForce(new Vector3(0, force, 0));
         }
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+        
+    //}
 
 }

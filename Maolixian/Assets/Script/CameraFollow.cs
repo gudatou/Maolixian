a29192710b281e2eac7b9eaad4b5bd71032c5c48
ILCeChio <52514283+ILCeChio@users.noreply.github.com>
2019-09-03 09:08:20 +0800

    zhamen
    闸门和镜头移动

---
+++ Maolixian/Assets/Script/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform cat;
+    private float distanceX;
+    // Use this for initialization
+    void Start()
+    {
+        distanceX = transform.position.x - cat.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            cat.position.x + distanceX,
+            transform.position.y,
+            transform.position.z);
+    }
+}

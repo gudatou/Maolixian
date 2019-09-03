f71d883d62d3a81c9d3a0413603655b5c2eb97f9
ILCeChio <52514283+ILCeChio@users.noreply.github.com>
2019-09-03 11:58:01 +0800

    0
    0

---
+++ Maolixian/Assets/Script/Carsport.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carsport : MonoBehaviour
+{
+    
+    public float speed = 2.0f;
+    public Rigidbody2D rig;
+    public  bool run = false;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+    }
+    
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Black Cat"))
+        {
+            run = true;
+           
+        }
+    }
+    
+    public void FixedUpdate()
+    {
+        if(run)
+        {
+            rig.velocity = new Vector2(speed, 0);
+        }
+        
+    }
+}
---
+++ Maolixian/Assets/Script/Tract1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tract1 : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("car"))
+        {
+            rig.velocity = new Vector2(0,0);
+        }
+    }
+}

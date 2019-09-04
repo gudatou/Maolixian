c7b2e8a93bb6983883e5e5067302d8709e7ddf24
ILCeChio <52514283+ILCeChio@users.noreply.github.com>
2019-09-04 16:02:57 +0800

    li

--- Maolixian/Assets/Script/Carsport.cs
+++ Maolixian/Assets/Script/Carsport.cs
@@ -4,40 +4,52 @@ using UnityEngine;
 
 public class Carsport : MonoBehaviour
 {
-    
+    public Rigidbody2D car = null;
     public float speed = 2.0f;
-    public Rigidbody2D rig;
-    public  bool run = false;
-  
+    //public Rigidbody2D rig;
+    public bool Rigidbody2D = false;
+    public bool run = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        car = GetComponent<Rigidbody2D>();
+        //rig = GetComponent<Rigidbody2D>();
+        //EC = GetComponent<EdgeCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       
+
+
     }
-    
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Black Cat"))
         {
             run = true;
-           
+
         }
+        //if(collision.gameObject .CompareTag("tractup"))
+        //{
+        //    GameObject.Find("chezhengxiang (1)").GetComponent<CircleCollider2D>().enabled = false;
+        //}
     }
-    
+
+
     public void FixedUpdate()
     {
-        if(run)
+        if (run == true)
         {
-            rig.velocity = new Vector2(speed, 0);
+            car.velocity = new Vector2(speed, car.velocity.y);
+            //    rig.velocity = new Vector2(speed,car.velocity.y);
         }
-        
+        //else
+        //{
+        //    car.velocity = new Vector2(-speed, car.velocity.y);
+        //}
+
     }
+
 }

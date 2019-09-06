b5a530916171ca985f62650df59b1b842d6926c9
ILCeChio <52514283+ILCeChio@users.noreply.github.com>
2019-09-06 10:51:41 +0800

    li
     car

--- Maolixian/Assets/Script/li/Tract1.cs
+++ Maolixian/Assets/Script/li/Tract1.cs
@@ -8,7 +8,7 @@ public class Tract1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
+        GetComponentInChildren<Transform>();
     }
 
     // Update is called once per frame

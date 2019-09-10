b0b14b11bdfcfcc6fc58874f2ea95bcb67e15151
ILCeChio <52514283+ILCeChio@users.noreply.github.com>
2019-09-10 11:38:41 +0800

    zhao
    111

---
+++ Maolixian/Assets/Script/zhao/fanhuizhucaidan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class fanhuizhucaidan : MonoBehaviour
+{
+    // Start is called before the first frame update\
+    //public void OnStartGame(int MainScence)
+    //{
+    //    Application.LoadLevel(1 - 1);
+    //}
+    public void OnStartGame(int SenceNumber)
+    {
+        Application.LoadLevel(SenceNumber);
+    }
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+
+
+    }
+    public void OnClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
